ab71dde6a5c59b6ab04df8c5dd5a07b5f0f4eb5e
Sandeep M S <sandeepms746@gmail.com>
2022-10-27 15:53:11 +0530

    added heading component

--- src/components/atoms/heading.tsx
+++ src/components/atoms/heading.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-interface headingProps {
-  headingLevel: any;
+interface headingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  headingLevel: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
   title: string;
 }
 
 const heading = (props: headingProps) => {
   const { headingLevel, title } = props;
 
-  const validHeadingLevels = ["h1", "h2", "h3", "h4", "h5", "h6"];
-  const safeHeading = headingLevel ? headingLevel.toLowerCase() : "";
-  const Title = validHeadingLevels.includes(safeHeading) ? safeHeading : "p";
+  //   const validHeadingLevels = ["h1", "h2", "h3", "h4", "h5", "h6"];
+  //   const safeHeading = headingLevel ? headingLevel.toLowerCase() : "";
+  //   const Title = validHeadingLevels.includes(headingLevel) ? headingLevel : "p";
+  const Title = headingLevel;
 
   return <Title>{title}</Title>;
 };
